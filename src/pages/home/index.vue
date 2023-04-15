2108bb0301d927b1d2b61bae6f0e52120f08e3f6
wjs <1530466272@qq.com>
2023-04-16 00:43:25 +0800

    feat: 下拉刷新和上拉加载

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,19 +3,22 @@
     <page-header />
     <main>
       <div class="main-content">
-        <image-card
-          v-for="item in imageList"
-          :img-src="item.url"
-          :thumbnail-src="item.thumbnailUrl"
-          :key="item.id"
-          :image-detail="item"
-          @click="handleClick"
-        />
-        <el-empty
-          v-if="!imageList.length"
-          description="这里空空如也 /(ㄒoㄒ)/~~"
-        />
-        <Loading v-if="loading" />
+        <PullToRefresh :onRefresh="refresh">
+          <image-card
+            v-for="item in imageList"
+            :img-src="item.url"
+            :thumbnail-src="item.thumbnailUrl"
+            :key="item.id"
+            :image-detail="item"
+            @click="handleClick"
+          />
+          <el-empty
+            v-if="!loading && !imageList.length"
+            description="这里空空如也 /(ㄒoㄒ)/~~"
+          />
+          <InfiniteScroll :loadMore="loadMoreData" :hasMore="hasMore" />
+        </PullToRefresh>
+        <Loading v-if="loading" text="数据加载中..." />
       </div>
       <image-modal
         :showModal.sync="showModal"
@@ -30,12 +33,21 @@
 import _ from "lodash";
 import PageHeader from "./components/PageHeader.vue";
 import ImageCard from "./components/ImageCard.vue";
+import PullToRefresh from "@/components/PullToRefresh";
+import InfiniteScroll from "@/components/InfiniteScroll";
 import ImageModal from "@/components/ImageModal";
 import Loading from "@/components/Loading";
 import { fetchAllPassFiles } from "@/api/file";
 export default {
   name: "Home",
-  components: { PageHeader, ImageCard, ImageModal, Loading },
+  components: {
+    PageHeader,
+    ImageCard,
+    ImageModal,
+    Loading,
+    PullToRefresh,
+    InfiniteScroll,
+  },
   created() {
     this.fetchData();
   },
@@ -45,19 +57,64 @@ export default {
       currentIndex: 0,
       showModal: false,
       loading: true,
+      total: 0,
+      pageInfo: {
+        pageNumber: 1,
+        pageSize: 5,
+      },
     };
   },
+  computed: {
+    hasMore() {
+      return this.imageList.length < this.total;
+    },
+  },
   methods: {
+    getData() {
+      return new Promise((resolve, reject) => {
+        fetchAllPassFiles(this.pageInfo)
+          .then(({ data }) => {
+            resolve(data);
+          })
+          .catch(reject);
+      });
+    },
     async fetchData() {
       try {
         this.loading = true;
-        const { data } = await fetchAllPassFiles();
-        const { files } = data;
+        const { files, count: total } = await this.getData();
+        this.total = total;
         this.imageList = files;
       } catch (error) {
         console.log(error);
       } finally {
         this.loading = false;
+      }
+    },
+    async refresh() {
+      // 下拉刷新 获取最新的图片数据
+      try {
+        // reset pageNumber
+        this.pageInfo.pageNumber = 1;
+        const { files, count: total } = await this.getData();
+        this.total = total;
+        this.imageList = files;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async loadMoreData(done) {
+      try {
+        this.pageInfo.pageNumber++;
+        const { files, count: total } = await this.getData();
+        this.total = total;
+        // 新加载的图片添加到imageList
+        this.imageList.push(...files);
+        done();
+      } catch (error) {
+        // 简单处理加载失败的情况
+        this.pageInfo.pageNumber--;
+        console.log(error);
       }
     },
     handleClick(image) {
@@ -71,12 +128,12 @@ export default {
 
 <style lang="scss" scoped>
 main {
-  margin-top: 60px;
-  padding-top: 20px;
+  padding-top: 60px;
   box-sizing: border-box;
   background-color: #eeeeee;
   box-sizing: border-box;
   .main-content {
+    padding-top: 10px;
     margin: 0 auto;
     width: 1000px;
     position: relative;
@@ -85,8 +142,7 @@ main {
 
 @media screen and (max-width: 767px) {
   main {
-    margin-top: 50px;
-    padding-top: 10px;
+    padding-top: 50px;
     .main-content {
       box-sizing: border-box;
       margin: 0 auto;
