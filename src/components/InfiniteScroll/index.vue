9bbbde0255b1114a304a695eb6fe8a5f4bf235d9
wjs <1530466272@qq.com>
2023-05-02 15:36:34 +0800

    feat: 优化InfiniteScroll组件 支持初始状态立即加载数据

--- src/components/InfiniteScroll/index.vue
+++ src/components/InfiniteScroll/index.vue
@@ -1,101 +1,139 @@
 <template>
   <div class="infinite-scroll">
-    <div v-if="!hasMore">没有更多了</div>
-    <div v-if="hasMore && loading" class="infinite-scroll__loader"></div>
+    <div class="infinite-scroll__content" ref="content">
+      <slot></slot>
+    </div>
+    <div class="infinite-scroll__placeholder" ref="placeholder">
+      <div class="infinite-scroll__loading" v-if="loading && hasMore">
+        <Loading>{{ loadingText }}</Loading>
+      </div>
+      <div class="infinite-scroll__finished" v-if="!hasMore">
+        {{ finishedText }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import _ from "lodash";
+import Loading from "../Loading";
+import { throttle } from "lodash";
 export default {
   name: "InfiniteScroll",
   props: {
-    loadMore: {
+    immediate: {
+      type: Boolean,
+      default: true,
+    },
+    onLoadMore: {
       type: Function,
       required: true,
     },
     threshold: {
       type: Number,
-      default: 30,
+      default: 50,
     },
     hasMore: {
       type: Boolean,
       required: true,
     },
+    loadingText: {
+      type: String,
+      required: false,
+      default: "正在加载",
+    },
+    finishedText: {
+      type: String,
+      required: false,
+      default: "没有更多了",
+    },
+  },
+  components: {
+    Loading,
   },
   data() {
     return {
       loading: false,
-      handleScrollThrottle: null,
+      onScroll: null,
+      observer: null,
     };
   },
   mounted() {
-    this.handleScrollThrottle = _.throttle(this.handleScroll, 200);
-    window.addEventListener("scroll", this.handleScrollThrottle);
-    /**
-     * TODO
-     * 实现类似ElementUI中的infinite-scroll-immediate功能
-     * 以防初始状态下内容无法撑满容器
-     */
+    const onScroll = throttle(this.handleScroll, 200);
+    window.addEventListener("scroll", onScroll);
+    this.onScroll = onScroll;
+
+    // 立即加载数据 以防初始状态下内容无法撑满容器
+    if (this.immediate) {
+      const observer = new MutationObserver(onScroll);
+      observer.observe(this.$refs.content, {
+        childList: true,
+        subtree: true,
+      });
+      onScroll();
+      this.observer = observer;
+    }
   },
   beforeDestroy() {
-    window.removeEventListener("scroll", this.handleScrollThrottle);
+    window.removeEventListener("scroll", this.onScroll);
+    if (this.observer) this.observer.disconnect();
+    this.observer = null;
   },
   methods: {
     handleScroll() {
-      if (!this.hasMore) return;
-      /**
-       *  这里只简单实现了功能
-       *  用InfiniteScroll组件自身到视口顶部的距离 减去 浏览器窗口的视口高度
-       *  得到组件距离视口底部的距离
-       *  ┌───────────┐
-       *  │  scoll    │
-       *  ├───────────┤
-       *  │           │
-       *  │  viewport │
-       *  │           │
-       *  ├───────────┤
-       *  |           |
-       *  │  IS Comp  │
-       *  ├───────────┤
-       */
-      const distanceToBottom =
-        this.$el.getBoundingClientRect().top - window.innerHeight;
-      if (distanceToBottom < this.threshold && !this.loading) {
-        this.loading = true;
-        this.loadMore(() => {
-          this.loading = false;
-        });
-      }
+      this.$nextTick(() => {
+        if (this.loading || !this.hasMore) return;
+
+        const scrollerHeight = window.innerHeight;
+        const placeholder = this.$refs.placeholder;
+        if (!scrollerHeight || !placeholder) return false;
+
+        const placeholderBottom = placeholder.getBoundingClientRect().bottom;
+        /**
+         *  计算placeholder到页面底部的距离
+         *  ┌───────────┐
+         *  │  scroll   │
+         *  ├───────────┤
+         *  │  viewport │
+         *  ├───────────┤
+         *  │placeholder│
+         *  ├───────────┤
+         */
+        const shouldTrigger =
+          // 滚动到了页面底部
+          Math.abs(placeholderBottom - scrollerHeight) <= this.threshold ||
+          // 初始状态下内容无法撑满容器
+          placeholderBottom <= scrollerHeight;
+
+        if (shouldTrigger) {
+          this.loading = true;
+          setTimeout(() => {
+            this.onLoadMore(() => {
+              this.loading = false;
+            });
+          }, 500);
+        } else if (this.observer) {
+          this.observer.disconnect();
+          this.observer = null;
+        }
+      });
     },
   },
 };
 </script>
 
 <style lang="scss">
-.infinite-scroll {
-  position: relative;
-  display: flex;
-  padding: 5px;
-  justify-content: center;
-  color: #999999;
-  font-size: 14px;
-  &__loader {
-    width: 25px;
-    height: 25px;
-    border-radius: 50%;
-    border: 2px solid rgba(0, 0, 0, 0.2);
-    border-top: 2px solid #000;
-    animation: infinite-scroll-spinner 0.8s linear infinite;
-  }
-}
+$text-color: #646566;
+$font-size: 14px;
+$height: 36px;
 
-@keyframes infinite-scroll-spinner {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
+.infinite-scroll {
+  height: $height;
+  &__loading,
+  &__finished {
+    color: $text-color;
+    font-size: $font-size;
+    line-height: $height;
+    text-align: center;
   }
 }
 </style>
